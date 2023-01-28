ad3de0a4170f7a89f6a88d509853e17f7b2fabc8
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2023-01-28 20:05:33 +0600

    data_types in python

---
+++ Data_Types.py
@@ -0,0 +1,32 @@
+# Python has the following data types built-in by default, in these categories:
+
+# Text Type:	str
+x = "Hello World"	# str
+
+# Numeric Types:	int, float, complex
+x = 20	#int	
+x = 20.5	#float	
+x = 1j	#complex	
+
+# Sequence Types:	list, tuple, range
+x = ["apple", "banana", "cherry"]	# list	
+x = ("apple", "banana", "cherry")	# tuple	
+x = range(6)	# range	
+
+# Mapping Type:	dict
+x = {"name" : "John", "age" : 36}	# dict	
+
+# Set Types:	set, frozenset
+x = {"apple", "banana", "cherry"}	# set	
+x = frozenset({"apple", "banana", "cherry"})	# frozenset	
+
+# Boolean Type:	bool
+x = True	# bool	
+
+# Binary Types:	bytes, bytearray, memoryview
+x = b"Hello"	# bytes	
+x = bytearray(5)	# bytearray	
+x = memoryview(bytes(5))	# memoryview	
+
+# None Type:	NoneType
+x = None
