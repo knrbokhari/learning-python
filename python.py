0a48019208fbb6f7ba151394c9c979078827d146
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2023-01-28 19:53:53 +0600

    first commit

---
+++ python.py
@@ -0,0 +1,15 @@
+# what is python
+
+# ans: Python is an interpreted, object-oriented, high-level programming language with dynamic semantics. Its high-level built in data structures, combined with dynamic typing and dynamic binding, make it very attractive for Rapid Application Development, as well as for use as a scripting or glue language to connect existing components together.
+
+# What are the built-in types available In Python?  
+
+# Answer
+# Common immutable type:
+# 1. numbers: int(), float(), complex()
+# 2. immutable sequences: str(), tuple(), frozenset(), bytes()
+# Common mutable type (almost everything else):
+# 1. mutable sequences: list(), bytearray()
+# 2. set type: set()
+# 3. mapping type: dict()
+# 4. classes, class instances
